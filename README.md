67ca37311efea91c1367530c596319e20c1f9e4f
pizza-bobby-joe <chriscarduck@gmail.com>
2023-11-12 22:14:43 -0500

    First Submission

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# cmsc508-fa2023-hw7-template
+# CMSC508-fa2023-hw7-template
+
+## Overview
+
+This project involves the creation of a company-wide resume database to capture the collective skills and certifications of all employees in the company. The database allows for tracking skills, employees, and their roles within the organization.
+
+## Table Schema
+
+### Skills Table
+- Columns: id, name, description, tag, url, time_commitment
+
+### People Table
+- Columns: id, first_name, last_name, email, linkedin_url, headshot_url, discord_handle, brief_bio, date_joined
+
+### PeopleSkills Table
+- Columns: id, skills_id, people_id, date_acquired
+
+### Roles Table
+- Columns: id, name, sort_priority
+
+### PeopleRoles Table
+- Columns: id, people_id, role_id, date_role_acquired
+
+## Database Setup (hw7-ddl.sql)
+
+The `hw7-ddl.sql` file in the [GitHub Repository](https://github.com/cmsc-vcu/cmsc508-fa2023-hw7-ChrisCarduck.git) provides instructions to:
+- Drop and recreate all tables.
+- Define primary keys and foreign keys.
+- Populate the skills, people, peopleskills, roles, and peopleroles tables with sample data.
+
+## HTML Report (hw7.qmd)
+
+The `hw7.qmd` file, rendered as an HTML document, includes:
+- Overview of the project and database.
+- Crows-foot diagram of the entities.
+- Listings of the people, roles, and skills tables.
+- Sample queries providing insights into the data.
+
+## Execution
+
+1. Clone the repository: `git clone https://github.com/cmsc-vcu/cmsc508-fa2023-hw7-ChrisCarduck.git`
+2. Set up the database using the `hw7-ddl.sql` file.
+3. Execute the queries and explore the results using the `hw7.qmd` file.
+
+## Dependencies
+
+- Python
+- Pandas
+- SQLAlchemy
+- Jupyter Notebook
+
+## Reflection
+
+Creating this database and associated reports was a challenging yet rewarding experience. The process of designing a database schema from the scaffold, considering the relationships between entities, and implementing it using SQL statements deepened my understanding of database management. The integration of Python within the Markdown document to execute SQL queries and display results was a novel approach, offering a practical bridge between theory and real-world implementation. Debugging potential errors in database connections and queries added a layer of realism to the project, emphasizing the importance of robust error handling. The Crows-foot diagram and HTML report provided a visual representation and clear output of the database structure and query results. Overall, this assignment highlighted the complexities of database design and implementation while reinforcing the importance of thorough testing and documentation.
+
+## Contributors
+
+- Chris Carduck
+
+Feel free to explore the database and generate reports using the provided HTML document!
--- hw7/hw7-ddl.sql
+++ hw7/hw7-ddl.sql
@@ -13,13 +13,21 @@
 # 6. Assign ID in the peopleskills and peopleroles automatically (use auto_increment)
 # 7. Data types: ONLY use "int", "varchar(255)", "varchar(4096)" or "date" as appropriate.
 
+
+
 # Section 1
 # Drops all tables.  This section should be amended as new tables are added.
 
-SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT=1;
+#SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS peopleroles;
+DROP TABLE IF EXISTS peopleskills;
 DROP TABLE IF EXISTS people;
-# ... 
-SET FOREIGN_KEY_CHECKS=1;
+DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS roles;
+#SET FOREIGN_KEY_CHECKS=1;
+
+
 
 # Section 2
 # Create skills( id,name, description, tag, url, time_commitment)
@@ -28,35 +36,90 @@ SET FOREIGN_KEY_CHECKS=1;
 # time committment offers some sense of how much time was required (or will be required) to gain the skill.
 # You can assign the skill descriptions.  Please be creative!
 
+DROP TABLE IF EXISTS skills;
+CREATE TABLE skills (
+    id int NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    tag VARCHAR(255) NOT NULL,
+    url VARCHAR(255) DEFAULT NULL,
+    time_commitment int DEFAULT NULL,
+    primary key (id)
+);
+
+
 
 # Section 3
 # Populate skills
 # Populates the skills table with eight skills, their tag fields must exactly contain “Skill 1”, “Skill 2”, etc.
 # You can assign skill names.  Please be creative!
 
+INSERT INTO skills ( id, name, description, tag ) values
+(1, 'Programming', 'Proficient in multiple programming languages, including Python, Java, and C++', 'Skill 1'),
+(2, 'Data Analysis', 'Skilled in data analysis and visualization using tools like Excel, R, and Tableau', 'Skill 2'),
+(3, 'Web Development', 'Experienced in creating dynamic and responsive web applications with HTML, CSS, and JavaScript', 'Skill 3'),
+(4, 'Database Management', 'Adept at designing and maintaining relational databases with SQL', 'Skill 4'),
+(5, 'Project Management', 'Strong project management skills with a focus on planning, execution, and team coordination', 'Skill 5'),
+(6, 'Digital Marketing', 'Proficient in digital marketing strategies, including SEO, SEM, and social media advertising', 'Skill 6'),
+(7, 'Graphic Design', 'Skilled in graphic design tools like Adobe Photoshop and Illustrator', 'Skill 7'),
+(8, 'Machine Learning', 'Knowledgeable in machine learning techniques and frameworks such as TensorFlow and scikit-learn', 'Skill 8');
+
 
 # Section 4
-# Create people( id,first_name, last_name, email, linkedin_url, headshot_url, discord_handle, brief_bio, date_joined)
+# Create people( people_id, people_first_name, people_last_name, email, linkedin_url, headshot_url, discord_handle, brief_bio, date_joined)
 # ID cannot be null, Last name cannot be null, date joined cannot be NULL.
 # All other fields can default to NULL.
 
+DROP TABLE IF EXISTS people;
 CREATE TABLE people (
-    people_id int,
-    people_last_name varchar(256) NOT NULL,
+    people_id int NOT NULL,
+    people_first_name VARCHAR(255) DEFAULT NULL,
+    people_last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) DEFAULT NULL,
+    linkedin_url VARCHAR(255) DEFAULT NULL,
+    headshot_url VARCHAR(255) DEFAULT NULL,
+    discord_handle VARCHAR(255) DEFAULT NULL,
+    brief_bio VARCHAR(255) DEFAULT NULL,
+    date_joined date DEFAULT (CURRENT_DATE),
     PRIMARY KEY (people_id)
 );
+
+
 
 # Section 5
 # Populate people with six people.
 # Their last names must exactly be “Person 1”, “Person 2”, etc.
 # Other fields are for you to assign.
 
-insert into people (people_id,people_last_name) values (1,'Person 1');
+INSERT INTO people (people_id, people_first_name, people_last_name, email) VALUES
+(1, 'Liam', 'Person 1', 'liam.Person1@gmail.com'),
+(2, 'Olivia', 'Person 2', 'olivia.Person2@gmail.com'),
+(3, 'Noah', 'Person 3', 'noah.Person3@gmail.com'),
+(4, 'Emma', 'Person 4', 'emma.Person4@gmail.com'),
+(5, 'Jackson','Person 5', 'jackson.Person5@gmail.com'),
+(6, 'Ava', 'Person 6', 'ava.Person6@gmail.com'),
+(7, 'Aiden', 'Person 7', 'aiden.Person7@gmail.com'),
+(8, 'Sophia', 'Person 8', 'sophia.Person8@gmail.com'),
+(9, 'Lucas', 'Person 9', 'lucas.Person9@gmail.com'),
+(10, 'Mia', 'Person 10', 'mia.Person10@gmail.com');
+
 
 
 # Section 6
 # Create peopleskills( id, skills_id, people_id, date_acquired )
 # None of the fields can ba NULL. ID can be auto_increment.
+
+DROP TABLE IF EXISTS peopleskills;
+CREATE TABLE peopleskills (
+id int auto_increment NOT NULL,
+skills_id int NOT NULL,
+people_id int NOT NULL,
+date_acquired date DEFAULT (CURRENT_DATE),
+PRIMARY KEY (id),
+FOREIGN KEY (skills_id) REFERENCES skills (id),
+FOREIGN KEY (people_id) REFERENCES people (people_id)
+);
+
 
 
 # Section 7
@@ -72,11 +135,32 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 9 has skills 2,5,6;
 # Person 10 has skills 1,4,5;
 # Note that no one has yet acquired skills 7 and 8.
- 
+
+INSERT INTO peopleskills ( people_id, skills_id ) VALUES
+(1,1), (1,3), (1,6),
+(2,3), (2,4), (2,5),
+(3,1), (3,5),
+(5,3), (5,6),
+(6,2), (6,3), (6,4),
+(7,3), (7,5), (7,6),
+(8,1), (8,3), (8,5), (8,6),
+(9,2), (9,5), (9,6),
+(10,1), (10,4), (10,5);
+
+
+
 
 # Section 8
 # Create roles( id, name, sort_priority )
 # sort_priority is an integer and is used to provide an order for sorting roles
+
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles (
+id int,
+name VARCHAR(255),
+sort_priority int,
+PRIMARY KEY (id)
+);
 
 
 
@@ -85,17 +169,37 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Designer, Developer, Recruit, Team Lead, Boss, Mentor
 # Sort priority is assigned numerically in the order listed above (Designer=10, Developer=20, Recruit=30, etc.)
 
+INSERT INTO roles ( id, name, sort_priority ) VALUES
+(1,'Designer',10),
+(2,'Developer',20),
+(3,'Recruit',30),
+(4,'Team Lead',40),
+(5,'Boss',50),
+(6,'Mentor',60);
+
 
 
 # Section 10
 # Create peopleroles( id, people_id, role_id, date_assigned )
 # None of the fields can be null.  ID can be auto_increment
 
+DROP TABLE IF EXISTS peopleroles;
+CREATE TABLE peopleroles (
+id int auto_increment NOT NULL,
+people_id int NOT NULL,
+role_id int NOT NULL,
+date_assigned date DEFAULT (CURRENT_DATE),
+PRIMARY KEY (id),
+FOREIGN KEY (people_id) REFERENCES people (people_id),
+FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+
 
 
 # Section 11
 # Populate peopleroles
-# Person 1 is Developer 
+# Person 1 is Developer
 # Person 2 is Boss, Mentor
 # Person 3 is Developer and Team Lead
 # Person 4 is Recruit
@@ -106,3 +210,14 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 9 is Developer
 # Person 10 is Developer and Designer
 
+INSERT INTO peopleroles ( people_id, role_id ) VALUES
+(1,2),
+(2,5), (2,6),
+(3,2), (3,4),
+(4,3),
+(5,3),
+(6,2), (6,1),
+(7,1),
+(8,1), (8,4),
+(9,2),
+(10,2), (10,1);
